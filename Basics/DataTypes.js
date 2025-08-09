7a7435ab60e1f0fa4d2ccdef7236b3a12a93715e
ELDER <bharatpratap12abh@gmail.com>
2025-08-09 10:02:36 +0000

    DataTypes

---
+++ Basics/DataTypes.js
@@ -0,0 +1,55 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+//Symbol() is designed to guarantee uniqueness, so two different calls can never be equal
+
+// const bigNumber = 3456543576654356754n 
+//we use n in the last of bignumber and javascript directly knows it's bigINT
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+//objects stored in scope{}
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+/*1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
+// https://262.ecma-international.org/5.1/#sec-11.4.3
